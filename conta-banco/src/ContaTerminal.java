ff3116e314447abe5c5cce87111790e6200a4d2a
João Gabriel FA <joaogabrielfa.dev@gmail.com>
2024-08-14 15:57:07 -0300

    Concluindo projeto proposto pela DIO

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -2,18 +2,25 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        int Numero = 1021;
-        String Agencia = "067-8";
-        String NomeCliente = "MARIO ANDRADE";
-        double Saldo = 237.48;
+        String Agencia;
+        String NomeCliente;
+        int Numero;
+        double Saldo;
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Por favor, digite o número da Agência !");
-        int numeroDigitado = sc.nextInt();
-        if (numeroDigitado == Numero) {
-            System.out.println("Olá " + NomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + Agencia + ", conta " + Numero + " e seu saldo de R$ " + Saldo + " já está disponível para saque.");
-        } else {
-            System.out.println("Conta não encontrada");
-        }
+
+        System.out.println("Por favor, digite o seu nome !");
+        NomeCliente = sc.next();
+        
+        System.out.println("Por favor, digite o número da agência !");
+        Agencia = sc.next();
+
+        System.out.println("Por favor, digite o número da conta !");
+        Numero = sc.nextInt();
+
+        System.out.println("Por favor, digite o saldo da conta !");
+        Saldo = sc.nextDouble();
+
+        System.out.println("Olá " + NomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + Agencia + ", conta " + Numero + " e seu saldo de R$ " + Saldo + " já está disponível para saque.");
     }
 }
